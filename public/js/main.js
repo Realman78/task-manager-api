5cb9465228159f2d26b8ba6b9add61387ff44dae
Realman78 <marin.dedic@me.com>
2021-01-01 20:27:20 +0100

    Edit task feature added

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,17 @@
 const userInfo = document.querySelector('h1')
 const avatarUser = document.querySelector('img')
 let user = undefined
-const taskList = document.querySelector('ul')
+const taskList = document.querySelector('ol')
 
-let taskDescriptions = []
+let taskObjects = []
+const checkboxes = []
 const addTaskButton = document.querySelector('#addTaskButton')
 const logoutButton = document.querySelector('#logoutButton')
 
 const cookies = document.cookie
 const token = cookies.substring(cookies.indexOf('token')+6)
 
-
+let lastEl = undefined
 
 window.onload = function(){
     if (token.length < 50){
@@ -28,13 +29,11 @@ function getUserAvatar(userID){
         }
     }).then(response => response.blob())
         .then(images => {
-            console.log(images)
-        
-        if (!URL.createObjectURL(images)){
-            avatarUser.remove()
-        }else{
-            avatarUser.src = URL.createObjectURL(images)
-        }
+            if (!URL.createObjectURL(images)){
+                avatarUser.remove()
+            }else{
+                avatarUser.src = URL.createObjectURL(images)
+            }
         }).catch((e)=>{
             console.log(e)
         })
@@ -66,7 +65,56 @@ function getUserInfo(){
     })
 }
 
+// taskList.onmouseover = (e)=>{
+//     if (e.target.nodeName === 'LI'){
+//         const index = taskObjects.findIndex((task)=>{
+//             return task.description === e.target.innerText
+//         })
+//         console.log(taskList.children[index])
+//         lastEl = taskList.children[index].id
+//         // const li = taskList.children[index]
+//         // const button = document.createElement('button')
+//         // button.innerText = 'HEOk'
+//         // li.appendChild(button)
+//         // console.log(taskList.children[index])
+//     }
+// }
+
+// taskList.onmouseleave = (e)=>{
+//     // const ol = e.target.children
+//     // const index = taskList.findIndex((li)=>{
+//     //     return task.description === e.target.innerText
+//     // })
+//     // console.log(index)
+//     console.log('ehokeofks' + lastEl)
+// }
+
+taskList.onclick = (e)=>{
+    
+    if (e.target.checked){
+        console.log(e)
+        
+        const index = taskObjects.findIndex((task)=>{
+            return task.description === e.path[1].id
+        })
+        const taskID = taskObjects[index]._id
+        fetch(`/tasks/${taskID}`,{
+            method: 'delete',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        }).then((res)=>{
+            setTimeout(()=>{
+                taskList.removeChild(e.path[1])
+            },1000)
+            
+        })
+    }
+}
+
 function getUserTasks(){
+    taskList.innerHTML = ''
     fetch('/tasks', {
         method: 'get',
         headers:{
@@ -75,15 +123,50 @@ function getUserTasks(){
         }
     }).then((res)=>{
         res.json().then((tasks)=>{
+            taskObjects = tasks
             tasks.forEach((task)=>{
-                taskDescriptions.push(task.description)
-                const li = document.createElement('li')
-                li.innerHTML = task.description
-                taskList.appendChild(li)
-            })
+                 const ch = document.createElement('input')
+                 ch.type = 'checkbox'
+                 ch.textContent = task.description
+                 checkboxes.push(ch)
+                 const li = document.createElement('li')
+                 li.id = task.description
+                 li.innerHTML = ch.outerHTML + ch.textContent
+                 const button = document.createElement('button')
+                 button.innerText = 'Edit task description'
+                 button.addEventListener('click', (e)=>{
+                     e.preventDefault()
+                     const td = prompt('Enter the new task description')
+                     if (td){
+                        const bodyData = JSON.stringify({
+                            description: td
+                        })
+                        const index = taskObjects.findIndex((task)=>{
+                            return e.path[1].id === task.description
+                        })
+                        const tID = taskObjects[index]._id
+                        fetch('/tasks/'+tID, {
+                            method: 'PATCH',
+                            headers:{
+                                'Content-Type': 'application/json',
+                                'Authorization': token
+                            },
+                            body: bodyData
+                        }).then((res)=>{
+                            getUserTasks()
+                            //location.reload()
+                        })
+                     }
+                     
+                 })
+                li.appendChild(button)
+                 taskList.appendChild(li)
+             })
         })
     })
 }
+
+//probaj sa divon
 
 addTaskButton.addEventListener('click', (e)=>{
     e.preventDefault()
@@ -103,11 +186,11 @@ addTaskButton.addEventListener('click', (e)=>{
         body: bodyData
     }).then((res)=>{
         res.json().then((data)=>{
-            const li = document.createElement('li')
-            li.innerHTML = data.description
-            taskList.appendChild(li)
+            // const li = document.createElement('li')
+            // li.innerHTML = data.description
+            // taskList.appendChild(li)
             
-            //location.reload()
+            getUserTasks()
         })
     })
     
@@ -129,6 +212,8 @@ logoutButton.addEventListener('click', (e)=>{
         })
     }
 })
+
+
 
 getUserInfo()
 getUserTasks()
